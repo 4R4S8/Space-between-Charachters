879807524231ed768747a36ea1e206832a1a2536
4R4S8 <arashzomorrodi24@gmail.com>
2021-11-13 15:50:50 +0330

    Add project files.

---
+++ Test The Array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test_The_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Entity: ");
+            string Word = Console.ReadLine();
+            for (int i = 0; i < Word.Length; i++)
+            {
+                Console.Write(" {0} ",Word[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
